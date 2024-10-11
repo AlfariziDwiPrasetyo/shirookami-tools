3d482e08d8f5cb5a6f44873fab05c9127b3e166b
Al Farizi Dwi Prasetyo <yoerdanatan@gmail.com>
2024-10-12 01:14:25 +0700

    feat: add link in main page

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,12 @@ function Home() {
             "group rounded-full border border-black/5 bg-neutral-100 text-xs text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
           )}
         >
-          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-            <span>Your lovely dev 💖</span>
-            <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-          </AnimatedShinyText>
+          <a href="https://shirookami.my.id">
+            <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+              <span>Your lovely dev 💖</span>
+              <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            </AnimatedShinyText>
+          </a>
         </div>
       </div>
       <BlurIn
